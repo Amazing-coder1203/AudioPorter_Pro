2c9ca593074bbade75aa6dab650fa44002e43abd
Shivkumar Devane <Amazing-coder1203@users.noreply.github.com>
2026-01-20 19:20:13 +0530

    Fix APK caching issue - bump cache version to v3

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'audioporter-v1';
+const CACHE_NAME = 'audioporter-v3';
 const ASSETS = [
     './',
     './index.html',
@@ -17,6 +17,17 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
     event.waitUntil(clients.claim());
 });
 
